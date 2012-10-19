eb5f38090d3a7b68f597cb87a1adcf4012f1e5e3
GinnyN <satelliteg@gmail.com>
2012-10-19 15:32:56 -0300

    Change of Address

--- tf2rpg/tf2rpg/settings.py
+++ tf2rpg/tf2rpg/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/xcontreras/Sites/tf2rpg/static/'
+STATIC_ROOT = '/Users/xcontreras/Sites/Team Fortress RPG Generators/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -107,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/xcontreras/Sites/tf2rpg/tf2rpg/templates"
+    "/Users/xcontreras/Sites/Team Fortress 2 RPG Generators/tf2rpg/templates"
 )
 
 INSTALLED_APPS = (
